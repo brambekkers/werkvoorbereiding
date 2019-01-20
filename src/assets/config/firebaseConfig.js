c23700898e4b55a6c443a7b872b2f15c784d03b4
Bram Bekkers <bram.bekkers@outlook.com>
2019-01-20 13:52:42 +0100

    Delete firebaseConfig.js

--- src/assets/config/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-	apiKey: "AIzaSyD1wD28RWqZhB7en4M1ACl7WxFVKW0LbeY",
-	authDomain: "werkvoorbereiding-hout.firebaseapp.com",
-	databaseURL: "https://werkvoorbereiding-hout.firebaseio.com",
-	projectId: "werkvoorbereiding-hout",
-	storageBucket: "werkvoorbereiding-hout.appspot.com",
-	messagingSenderId: "1033483014946"
-}
