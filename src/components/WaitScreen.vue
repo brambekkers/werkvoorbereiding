7b4538c8e884a35774a4572184544b268cb727d5
brambekkers <bram.bekkers@outlook.com>
2020-04-08 12:08:22 +0200

    admin

--- src/components/WaitScreen.vue
+++ src/components/WaitScreen.vue
@@ -1,33 +1,34 @@
 <template>
-	<div class="loadingScreen">
-		<i class="loadingIcon fas fa-spinner fa-spin"></i>
-		<p>"De resultaten worden omgezet naar een afbeelding. Even geduld..."</p>
-	</div>
+    <div class="loadingScreen">
+        <i class="loadingIcon fas fa-spinner fa-spin"></i>
+        <p>{{msg}}</p>
+    </div>
 </template>
 
 <script>
 export default {
-	name: 'WaitScreen'
+    name: "WaitScreen",
+    props: ["msg"]
 };
 </script>
 
 <style scoped>
 .loadingScreen {
-	position: fixed;
-	padding: 2rem;
-	left: 0;
-	top: 0;
-	z-index: 99;
-	height: 100%;
-	width: 100%;
-	background-color: rgba(255, 255, 255, 0.6);
-	text-align: center;
+    position: fixed;
+    padding: 2rem;
+    left: 0;
+    top: 0;
+    z-index: 99;
+    height: 100%;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.6);
+    text-align: center;
 }
 
 .loadingIcon {
-	margin: auto;
-	margin-top: 30vh;
-	margin-bottom: 5vh;
-	font-size: 5rem;
+    margin: auto;
+    margin-top: 30vh;
+    margin-bottom: 5vh;
+    font-size: 5rem;
 }
 </style>
